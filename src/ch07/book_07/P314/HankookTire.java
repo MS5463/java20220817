8708aa0bc9019de90c10fc5e0ce47bdf8923b0ce
MS5463 <110441724+MS5463@users.noreply.github.com>
2022-08-31 16:58:34 +0900

    Update HankookTire.java

--- src/ch07/book_07/P314/HankookTire.java
+++ src/ch07/book_07/P314/HankookTire.java
@@ -3,5 +3,9 @@ package ch07.book_07.P314;
 public class HankookTire extends Tire {
 	//필드 
 	//생성자
-	public HankookTire(String location, int maxRotation)
+	public HankookTire(String location, int maxRotation) {
+		super(location, maxRotation);
+	}
+	//메소드
+	@o
 }
